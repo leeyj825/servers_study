372a17e7b1443f6a5a9aab4d1a9489aee655afdd
Lee YuJeong <uh5213@naver.com>
2021-05-04 16:13:33 +0900

    init commit

--- src/main/java/kr/ac/jbnu/ampm/leeyj/Controller.java
+++ src/main/java/kr/ac/jbnu/ampm/leeyj/Controller.java
@@ -4,25 +4,83 @@ package kr.ac.jbnu.ampm.leeyj;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<?> responseEntity(HttpServletRequest request){
+    public ResponseEntity<?> responseEntity(HttpServletRequest request, @PathVariable String id){
         ResponseEntity<?> responseEntity = null;
 
-        responseEntity = new ResponseEntity<>("OK", HttpStatus.OK);
+        Map<String, Object> voMap = null;
 
-        Logger logger = LoggerFactory.getLogger("kr.ac.jbnu.ampm.leeyj.Controller");
-        logger.info("log");
+        if (id != null && !id.equals("")) {
+            voMap = new HashMap<String, Object>();
+
+            voMap.put("books", new LinkedHashMap<String, Object>() {{ //linked hashmap : 순서 보장.
+
+                put("book3", new HashMap<String , Object>(){{
+                    put("name", "디지털공학개론");
+                }});
+
+            put("book2", "소프트웨어공학개론");
+            put("book1", "마션");
+            }});
+
+            voMap.put("name", "이유정");
+            voMap.put("age", 22);
+
+            responseEntity = new ResponseEntity<>(voMap, HttpStatus.OK);
+
+
+        }
+        else{
+
+            responseEntity = new ResponseEntity<>("not found", HttpStatus.NOT_FOUND);
+        }
+
         return responseEntity;
     }
 }
+
+/*
+{
+	{
+	"books" : {
+	  "book3" : {
+			"name" : "디지털공학개론"
+	  },
+	  "book2" : "소프트웨어공학개론"
+	  "book1" : "마션"
+	},
+	"name": "이유정"
+    "age" : "22"
+}
+}
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
